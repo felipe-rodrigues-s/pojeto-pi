52978358e3787ab912efcf944dc8cdcfa2374680
gervasioartur <gervasioartur@outlook.com.br>
2022-02-10 21:15:44 -0200

    Estruturando o banckend e frontend

---
+++ frontend/public/js/srcipt.js
@@ -0,0 +1,48 @@
+var slideIndex = 0;
+showSlides();
+
+function plusSlides(n) {
+  showSlidesBiPosition(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlidesBiPosition(slideIndex = n);
+}
+
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
+
+var slideIndex = 1;
+showSlidesBiPosition(slideIndex);
+
+function showSlidesBiPosition(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
