b96b424a088d424c443e8b06cfb69bd53bf08f6c
Felipe <1felipersilva1@gmail.com>
2022-05-10 21:03:20 -0300

    reverção da Alteração na rota

--- packages/web/src/Routes.jsx
+++ packages/web/src/Routes.jsx
@@ -26,7 +26,7 @@ export function AppRoutes() {
           <Route exact path="/cart" element={<Private><Cart /></Private>} />
           <Route exact path="/catalogo" element={<Catalogo />} />
           <Route exact path="/product" element={<Product />} />
-          <Route exact path="projeto-pi-nine.vercel.app/login" element={<Login />} />
+          <Route exact path="/login" element={<Login />} />
           <Route exact path="/teclados" element={<ProductsCategory />} />
           <Route exact path="/mouses" element={<ProductsCategory />} />
           <Route exact path="/headsets" element={<ProductsCategory />} />
